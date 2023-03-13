01c535f626ef6b494bfb247c3d4cc31943552f96
Bohdan <ntrxhm@gmail.com>
2023-03-13 09:44:42 +0200

    Initial version

---
+++ checkSize.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEditor;
+#if UNITY_EDITOR
+[CustomEditor(typeof(checkSizeScript))]
+public class checkSize : Editor
+{
+
+    //public float height = 0f;
+    public float width = 0f;
+    public float lengh = 0f;
+    public float mass = 0f;
+    public float engine = 0f;
+
+    public override void OnInspectorGUI() {
+        checkSizeScript myScript = (checkSizeScript)target;
+
+        //float height = 0f;
+        myScript.height = EditorGUILayout.FloatField("Height", myScript.height);
+        myScript.width = EditorGUILayout.FloatField("Width", myScript.width);
+        myScript.lengh = EditorGUILayout.FloatField("Length", myScript.lengh);
+        myScript.mass = EditorGUILayout.FloatField("Mass", myScript.mass);
+        myScript.engine = EditorGUILayout.FloatField("Engine", myScript.engine);
+
+        if (GUILayout.Button("Create SizerBox")) {
+            myScript.Button_Press();
+
+        }
+
+        if (GUILayout.Button("Remove SizerBox")) {
+            myScript.Button_Remove();
+        }
+    }
+}
+#endif      
---
+++ checkSizeScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class checkSizeScript : MonoBehaviour
+{
+    public float height;
+    public float width;
+    public float lengh;
+    public float mass;
+    public float engine;
+
+    GameObject sizer;
+
+
+    public void Button_Press() {
+        sizer = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        
+        sizer.transform.position = transform.position;
+        sizer.transform.localScale = new Vector3(width / 1000F, height / 1000F, lengh / 1000F);
+        sizer.transform.parent = transform;
+    }
+
+    public void Button_Remove() {
+        DestroyImmediate(sizer);
+    }
+}
